a7338765b937807e71a9e6cc228fa50c5c695d2f
Brynn <brynnreginajohnson@gmail.com>
2021-02-09 13:47:01 -0600

    starting components - App, modal, root

---
+++ friendbook/frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <Modal />
+        <header>Welcome to Friendbook</header>
+        
+    </div>
+);
--- friendbook/frontend/friendbook.jsx
+++ friendbook/frontend/friendbook.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {signup, login, logout} from "./util/session_api_util"
+import configureStore from './store/store'
 
-window.signup = signup
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to Friendbook</h1>, root);
 });
