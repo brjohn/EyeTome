25ce06f965e49944bc5d3acc5afcb6fa8f2172fd
Brynn <brynnreginajohnson@gmail.com>
2021-02-09 17:57:56 -0600

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
 
 users = User.create([
-    {first_name: "Ginger", last_name: "Spice", birthday: 01-04-1980, gender: "Female", email: "spiceworld@gmail.com"},
-    {first_name: "Bill", last_name: "Nye", birthday: 04-12-1955, gender: "Male", email: "scienceguy@gmail.com"},
-    {first_name: "Miss", last_name: "Frizzle", birthday: 12-10-1960, gender: "Female", email: "magicschoolbus@gmail.com"},
-    {first_name: "Elle", last_name: "Woods", birthday: 05-07-1985, gender: "Female", email: "deltanu@gmail.com"},
+    {first_name: "Bill", last_name: "Nye", birthday: "04-12-1955", gender: "Male", email: "scienceguy@gmail.com", password: "123456"},
+    {first_name: "Miss", last_name: "Frizzle", birthday: "12-10-1960", gender: "Female", email: "magicschoolbus@gmail.com", password: "123456"},
+    {first_name: "Elle", last_name: "Woods", birthday: "05-07-1985", gender: "Female", email: "deltanu@gmail.com", password: "123456"},
 ])
+
+ginger = User.create({first_name: "Ginger", last_name: "Spice", birthday: "01-04-1980", gender: "Female", email: "spiceworld@gmail.com", password: "123456"})
