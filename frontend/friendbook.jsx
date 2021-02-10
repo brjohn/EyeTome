45c8cbb569a20f66fbb51ff09fc7a13a0b14c29c
Brynn <brynnreginajohnson@gmail.com>
2021-02-10 13:01:27 -0600

    tested signup, login, logout on chrome console - all working

--- frontend/friendbook.jsx
+++ frontend/friendbook.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import {signup, login, logout} from "./util/session_api_util"
+import {signup, login, logout} from "./actions/session_actions"
 // import 
 import configureStore from './store/store';
 import Root from './components/root';
@@ -8,8 +8,14 @@ import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const store = configureStore();
+    
     window.getState = store.getState;
     window.dispatch = store.dispatch;
+   window.signup = signup;
+   window.logout = logout;
+   window.login = login;
+    
+    
     const root = document.getElementById("root");
     ReactDOM.render(<Root store={store}/>, root);
 });
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -5,11 +5,12 @@ const _nullSession = {
 };
 
 const sessionReducer = (oldState = _nullSession, action) => {
+    
     Object.freeze(oldState);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            const currentUser = action.user;
-            return Object.assign({}, {currentUser})
+            const currentUser = action.currentUser;
+            return Object.assign({}, {currentUser: currentUser.id})
         case LOGOUT_CURRENT_USER:
             return _nullSession;
         default:
