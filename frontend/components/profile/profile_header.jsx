03da657c04f1169461a31a61154183b204f02929
Brynn <brynnreginajohnson@gmail.com>
2021-02-17 17:38:54 -0600

    profile header css

--- frontend/components/profile/profile_header.jsx
+++ frontend/components/profile/profile_header.jsx
@@ -4,18 +4,22 @@ const ProfileHeader = (props) => {
 
     return (
         <div className="profile-header">
-            <div className="cover-photo-wrapper"></div>
-            <div className="profile-pic">
+            <div className="cover-photo-wrapper">
+                <div className="cover-photo"></div>
                 <div className="profile-circle"></div>
-                <div className="photo-edits">
-                    <button id="profile-pic-edit">
-                        <i className="fas fa-camera"></i>
-                    </button>
-                    <button>
-                        <i className="fas fa-camera"></i>
-                        <p>Edit Cover Photo</p>
-                    </button>
+                <div className="pic-edit-banner">
+                    {/* <div className="profile-circle"></div> */}
+                    <div className="photo-edits">
+                        <button id="profile-pic-edit">
+                            <i className="fas fa-camera"></i>
+                        </button>
+                        <button id="cover-photo-edit">
+                            <i className="fas fa-camera"></i>
+                            <p>Edit Cover Photo</p>
+                        </button>
+                    </div>
                 </div>
+                
             </div>
             <div className="name-banner">
                 <h2>{props.profileOwner.first_name} {props.profileOwner.last_name}</h2>
