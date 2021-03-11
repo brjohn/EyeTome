67e3957800cd2f00c8af0f3c10f2e70867ad3872
Brynn <brynnreginajohnson@gmail.com>
2021-03-10 20:05:26 -0600

    test backend api calls

--- app/controllers/api/requests_controller.rb
+++ app/controllers/api/requests_controller.rb
@@ -5,7 +5,7 @@ class Api::RequestsController < ApplicationController
         if @request.save 
             render "api/requests/show"
         else
-            render json: @request.errors.full_messages, status 422
+            render json: @request.errors.full_messages, status: 422
         end
     end
 
@@ -15,7 +15,8 @@ class Api::RequestsController < ApplicationController
             @request.destroy 
             render "api/requests/show"
         else
-            render json: ["Request not found"], status 404
+            render json: ["Request not found"], status: 404
+        end
     end
 
     private 
