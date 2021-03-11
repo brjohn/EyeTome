ce7beae94d8ea05cbd875fb432e5dbb651fbc063
Brynn <brynnreginajohnson@gmail.com>
2021-03-10 19:55:16 -0600

    change views

---
+++ app/controllers/api/requests_controller.rb
@@ -0,0 +1,25 @@
+class Api::RequestsController < ApplicationController
+
+    def create 
+        @request = Request.new(request_params)
+        if @request.save 
+            render "api/requests/show"
+        else
+            render json: @request.errors.full_messages, status 422
+        end
+    end
+
+    def destroy 
+        @request = Request.find_by(id: params[:id])
+        if @request 
+            @request.destroy 
+            render "api/requests/show"
+        else
+            render json: ["Request not found"], status 404
+    end
+
+    private 
+    def request_params
+        params.require(:request).permit(:requester_id, :friend_requested_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do 
     resources :users 
+    resources :friendships, only: [:create, :destroy]
+    resources :requests, only: [:create, :destroy]
     resource :session, only: [:create, :destroy, :show]
   end
   
