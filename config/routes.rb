f6a849457900057efab71e0afdc81bd23304a5eb
Brynn <brynnreginajohnson@gmail.com>
2021-03-23 19:59:57 -0500

    add post controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do 
     resources :users 
+    resources :posts
     resources :friendships, only: [:create, :destroy]
     resources :requests, only: [:create, :destroy]
     resource :session, only: [:create, :destroy, :show]
