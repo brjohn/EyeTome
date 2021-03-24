3feab1f2e2a42c403c2507ce3da3160e3dc17a1e
Brynn <brynnreginajohnson@gmail.com>
2021-03-23 19:59:57 -0500

    add post controller methods

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -3,12 +3,41 @@ class Api::PostsController < ApplicationController
 
     #remember to make a way to capture the wall_owner_id!!!
 
+    def index
+        @posts = Post.all 
+        render :index
+    end
+
+    def show 
+        @post = Post.find_by(id: params[:id])
+        render :show
+    end
+
     def create
-        @post = current_user.posts.new(post_params)
+        @post = Post.new(post_params)
         if @post.save
             render :show
         else
-            render json: @post, status: :unprocessable_entity
+            render json: @post.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @post = Post.find_by(id: params[:id]) 
+        if @post.update(post_params)
+            render :show 
+        else
+            render json: @post.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy 
+        @post = Post.find_by(id: params[:id])
+        if @post 
+            @post.destroy 
+            render :index 
+        else
+            render json: ['Cannot find or delete post'], status: 422
         end
     end
 
