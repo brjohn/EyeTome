bb1b9363b655f174f02501c0e79f3902fd263e45
Brynn <brynnreginajohnson@gmail.com>
2021-03-28 13:03:38 -0500

    create post modal on profile page - pic upload inexplicably broken. Works on newsfeed

--- frontend/components/posts/post_list.jsx
+++ frontend/components/posts/post_list.jsx
@@ -9,22 +9,40 @@ const postPic = (post) => {
     }
 }
 
+
 const PostList = (props) => {
-        let allPosts = props.posts
+    let allPosts = props.posts
+
+    const displayPostOptionsIcon = (post) => {
+        if (post.poster.id === props.currentUser ){
+            return (
+                <div className="edit-post-icon">
+                    
+                        <i className="fas fa-ellipsis-h"></i>
+                    
+                </div>
+            )
+        } else {
+            return null;
+        }        
+    }
 
         return (
             <ul className="all-nf-posts">
                 {allPosts.reverse().map((post, idx) => {
                     return (
                         <li className="post" key={idx}>
-                            <div className="post-header">
+                            
+                            <div className="post-header-list">
+                                <div className="post-header-left">
                                 <Link to={`/users/${post.poster.id}`} className="poster-thumbnail">
                                     <i className="fas fa-user-circle"></i> 
                                 </Link>
                                 <Link to={`/users/${post.poster.id}`} className="poster-name">
                                     <div>{post.poster.first_name} {post.poster.last_name}</div> 
                                 </Link>
-                    
+                                </div>
+                                {displayPostOptionsIcon(post)}
                             </div>
                             <div className="post-text">
                                 {post.body}
