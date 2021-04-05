3537dbe0f543392f5f8e6c8619f2780f84081a11
Brynn <brynnreginajohnson@gmail.com>
2021-04-05 14:31:31 -0400

    create request button container, add to profile

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -2886,6 +2886,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _session_form_modal__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../session_form/modal */ "./frontend/components/session_form/modal.jsx");
 /* harmony import */ var _posts_post_list__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../posts/post_list */ "./frontend/components/posts/post_list.jsx");
 /* harmony import */ var _posts_create_post_box__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../posts/create_post_box */ "./frontend/components/posts/create_post_box.jsx");
+/* harmony import */ var _requests_request_button_container__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../requests/request_button_container */ "./frontend/components/requests/request_button_container.jsx");
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -2909,6 +2910,7 @@ function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Re
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 // when profile renders, navbar home icon and profile button change colors
+
 
 
 
@@ -2947,9 +2949,8 @@ var Profile = /*#__PURE__*/function (_React$Component) {
           className: "fas fa-pencil-alt"
         }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Edit Profile"));
       } else {
-        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-          className: "edit-button-height"
-        });
+        // return <div className="edit-button-height"></div>;
+        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_requests_request_button_container__WEBPACK_IMPORTED_MODULE_6__["default"], null);
       }
     }
   }, {
@@ -3445,6 +3446,99 @@ var mapDispatchToProps = function mapDispatchToProps(dispatch) {
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_0__["connect"])(mapStateToProps, mapDispatchToProps)(_update_profile_form__WEBPACK_IMPORTED_MODULE_2__["default"]));
+
+/***/ }),
+
+/***/ "./frontend/components/requests/request_button.jsx":
+/*!*********************************************************!*\
+  !*** ./frontend/components/requests/request_button.jsx ***!
+  \*********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+
+
+var RequestButton = /*#__PURE__*/function (_React$Component) {
+  _inherits(RequestButton, _React$Component);
+
+  var _super = _createSuper(RequestButton);
+
+  function RequestButton(props) {
+    _classCallCheck(this, RequestButton);
+
+    return _super.call(this, props);
+  }
+
+  _createClass(RequestButton, [{
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", null, "Request Button");
+    }
+  }]);
+
+  return RequestButton;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+
+/* harmony default export */ __webpack_exports__["default"] = (RequestButton);
+
+/***/ }),
+
+/***/ "./frontend/components/requests/request_button_container.jsx":
+/*!*******************************************************************!*\
+  !*** ./frontend/components/requests/request_button_container.jsx ***!
+  \*******************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
+/* harmony import */ var _actions_request_actions__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../actions/request_actions */ "./frontend/actions/request_actions.js");
+/* harmony import */ var _actions_friendship_actions__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../actions/friendship_actions */ "./frontend/actions/friendship_actions.js");
+/* harmony import */ var _request_button__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./request_button */ "./frontend/components/requests/request_button.jsx");
+
+
+
+
+
+var mapDispatchToProps = function mapDispatchToProps(dispatch) {
+  return {
+    createRequest: function createRequest(request) {
+      return dispatch(Object(_actions_request_actions__WEBPACK_IMPORTED_MODULE_1__["createRequest"])(request));
+    },
+    deleteFriendship: function deleteFriendship(id) {
+      return dispatch(Object(_actions_friendship_actions__WEBPACK_IMPORTED_MODULE_2__["deleteFriendship"])(id));
+    }
+  };
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_0__["connect"])(null, mapDispatchToProps)(_request_button__WEBPACK_IMPORTED_MODULE_3__["default"]));
 
 /***/ }),
 
---
+++ frontend/components/requests/request_button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class RequestButton extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        return(
+            <button>Request Button</button>
+        )
+    }
+}
+
+export default RequestButton;
