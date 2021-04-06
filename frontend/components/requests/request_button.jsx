7cc85bcbc2d2df51427a44f0d908b6f7ff668f41
Brynn <brynnreginajohnson@gmail.com>
2021-04-05 20:29:08 -0400

    add working send request and unfriend button to profile

--- frontend/components/requests/request_button.jsx
+++ frontend/components/requests/request_button.jsx
@@ -3,12 +3,39 @@ import React from 'react';
 class RequestButton extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            friendStatus: 'Add Friend'
+        }
+        this.friendAction = this.friendAction.bind(this)
+    }
+    componentDidMount(){
+        let {profileOwner, fullCurrentUser} = this.props
+        if (profileOwner.friendships && Object.values(profileOwner.friendships).some(friend => friend.id === fullCurrentUser.id)){
+            this.setState({['friendStatus']: 'Unfriend'})
+        }
+    }
+
+    friendAction(){
+        let {profileOwner, fullCurrentUser} = this.props
+        if (this.state.friendStatus === 'Add Friend'){
+            let request = {};
+            request.requestor_id = fullCurrentUser.id;
+            request.friend_requested_id = profileOwner.id;
+            this.props.createRequest(request);
+            this.props.fetchUser(fullCurrentUser.id);
+            this.setState({['friendStatus']: 'Request Sent'})
+
+        } else if (this.state.friendStatus === 'Unfriend'){
+            let str = Object.keys(profileOwner.friendships).find(key => profileOwner.friendships[key].id === fullCurrentUser.id)
+            this.props.deleteFriendship(parseInt(str))
+            this.setState({['friendStatus']: 'Add Friend'})
+        }
     }
 
     render(){
 
         return(
-            <button>Request Button</button>
+            <button onClick={this.friendAction}>{this.state.friendStatus}</button>
         )
     }
 }
