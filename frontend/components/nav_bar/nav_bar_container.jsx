354864cace9909019580c781acf10790969f27cb
Brynn <brynnreginajohnson@gmail.com>
2021-04-05 20:50:56 -0400

    add request count to bell, must change nav bar to a class component so I can fetch current user on mount

--- frontend/components/nav_bar/nav_bar_container.jsx
+++ frontend/components/nav_bar/nav_bar_container.jsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
+import { fetchUser } from '../../actions/user_actions';
 import NavBar from './nav_bar';
 
 const mapStateToProps = ({ session, entities: {users}}) => ({
@@ -10,7 +11,8 @@ const mapStateToProps = ({ session, entities: {users}}) => ({
 
 const mapDispatchToProps = dispatch => ({
     
-    logout: () => dispatch(logout())
+    logout: () => dispatch(logout()),
+    fetchUser: (id) => dispatch(fetchUser(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
